1757579de055b3c550190d91bfe884dff197c234
Stablest <marlonlocci@hotmail.com>
2023-06-03 00:00:54 -0300

    Languages and currencies are now displaying properly. Added key to map Border. Added desktop CSS and dark mode css. Added allCountries ref. Removed loading text.

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -6,24 +6,31 @@ import { Template } from "@/components/Template"
 import { URL } from "@/utils/const/enums/url"
 import { acessObjectKey } from "@/utils/functions/acessObjectKey"
 import { CountryType, Currencies, NativeName } from "@/utils/interfaces/CountryTypes"
-import { Suspense, useEffect, useState } from "react"
+import Link from "next/link"
+import { Suspense, useEffect, useRef, useState } from "react"
 
 type PageProps = {
     params: { [key: string]: string },
 }
 
 const Page = ({ params }: PageProps) => {
-    const { name } = params
-
+    const name = params.name.replace('%20', ' ')
+    const allCountries = useRef<CountryType[]>([])
     const [country, setCountry] = useState<CountryType | null>(null)
 
     useEffect(() => {
         try {
-            fetch(URL.NAME + name + '?fullText=true').then(
+            fetch(URL.ALL).then(
                 res => {
                     return res.json().then(
                         (data: CountryType[]) => {
-                            setCountry(data.pop() ?? null)
+                            const newCountries: CountryType[] = []
+                            data.forEach((element: CountryType, index: number) => {
+                                newCountries.push(element)
+                                if (element.name.common === name)
+                                    setCountry(element)
+                            })
+                            allCountries.current = newCountries
                         }
                     )
                 }
@@ -34,39 +41,43 @@ const Page = ({ params }: PageProps) => {
     }, [])
 
     return (
-        <main className="px-8 h-[200vh]">
+        <main className="px-8 xl:px-20 overflow-auto h-auto">
             {country ?
                 <>
-                    <a href="/" className="flex justify-center items-center gap-x-4 bg-white w-32 h-12 rounded-sm my-8 drop-shadow-md">
-                        <img src="./arrow-left.svg" alt="Left arrow" className="w-6" />
+                    <Link href="/" className="flex justify-center items-center gap-x-4 dark:bg-dark-blue bg-white w-32 h-12 rounded-sm my-8 drop-shadow-md">
+                        <img src="./arrow-left.svg" alt="Left arrow" className="w-6 brightness-[0] saturate-[0%] invert-[100%] hue-rotate-[0deg] contrast-[180%]" />
                         <div>Back</div>
-                    </a>
-                    <img src={country.flags[0]} alt={`Flag of ${country.name.common}`} className="my-12 mt-16 max-w-xl w-full" />
-                    <div className="text-xl font-extrabold my-6">{country.name.common}</div>
-                    <div className="flex flex-col gap-y-8">
-                        <div className="flex flex-col gap-y-2">
-                            <Pair title="Native Name:">{acessObjectKey<NativeName>(country.name.nativeName, 0).common}</Pair>
-                            <Pair title="Population:">{country.population.toString()}</Pair>
-                            <Pair title="Region:">{country.region}</Pair>
-                            <Pair title="Sub Region:">{country.subregion}</Pair>
-                            <Pair title="Capital:">{country.capital[0]}</Pair>
+                    </Link>
+                    <div className="xl:flex xl:justify-center xl:items-center xl:gap-x-16">
+                        <img src={country.flags[0]} alt={`Flag of ${country.name.common}`} className="my-12 mt-16 max-w-xl w-full" />
+                        <div>
+                            <div className="text-xl font-extrabold my-6">{country.name.common}</div>
+                            <div className="flex flex-col gap-y-8 xl:flex-row xl:gap-x-24">
+                                <div className="flex flex-col gap-y-2">
+                                    <Pair title="Native Name:">{acessObjectKey<NativeName>(country.name.nativeName, 0).common}</Pair>
+                                    <Pair title="Population:">{country.population.toLocaleString('pt-BR')}</Pair>
+                                    <Pair title="Region:">{country.region}</Pair>
+                                    <Pair title="Sub Region:">{country.subregion}</Pair>
+                                    <Pair title="Capital:">{country.capital[0]}</Pair>
+                                </div>
+                                <div className="flex flex-col gap-y-2">
+                                    <Pair title="Top Level Domain:">{country.tld.toString()}</Pair>
+                                    <Pair title="Currencies:">{Object.keys(country.currencies).map((_, index, array) =>
+                                        acessObjectKey(country.currencies, index).name + (index + 1 !== array.length ? ', ' : '')
+                                    )}</Pair>
+                                    <Pair title="Languages:">{Object.keys(country.languages).map((_, index, array) =>
+                                        acessObjectKey(country.languages, index) + (index + 1 !== array.length ? ', ' : '')
+                                    )}</Pair>
+                                </div>
+                            </div>
+                            <div className="font-semibold mt-8 my-4">Border Countries:</div>
+                            <div className="flex flex-wrap gap-4 my-4 mb-8">
+                                {country.borders.map((element, index) => <Border key={index} allCountries={allCountries.current}>{element}</Border>)}
+                            </div>
                         </div>
-                        <div className="flex flex-col gap-y-2">
-                            <Pair title="Top Level Domain:">{country.tld.toString()}</Pair>
-                            <Pair title="Currencies:">{Object.keys(country.currencies).map((_, index) =>
-                                acessObjectKey(country.currencies, index).name
-                            )}</Pair>
-                            <Pair title="Languages:">{Object.keys(country.languages).map((_, index) =>
-                                acessObjectKey(country.languages, index)
-                            )}</Pair>
-                        </div>
-                    </div>
-                    <div className="font-semibold mt-8 my-4">Border Countries:</div>
-                    <div className="flex flex-wrap gap-4">
-                        {country.borders.map((element) => <Border>{element}</Border>)}
                     </div>
                 </> :
-                <div className="mx-auto py-16 text-4xl font-extrabold">Loading...</div>}
+                <div ></div>}
         </main>
     )
 }
