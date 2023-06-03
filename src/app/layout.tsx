f7cb4ba7a84fe7995bd0faad59e6db01198f601a
Stablest <marlonlocci@hotmail.com>
2023-06-03 00:04:40 -0300

    Removed commented out code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,22 +8,11 @@ export const metadata = {
   description: 'All about countries',
 }
 
-// async function getData() {
-//   try {
-//     const res = await fetch(URL.ALL)
-//     const data: CountryType[] = await res.json()
-//     return data
-//   } catch (e) {
-//     console.error(e)
-//   }
-// }
-
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  // const data = await getData()
 
   return (
     <html lang="en">
