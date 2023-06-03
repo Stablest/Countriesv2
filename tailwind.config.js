c041c09891ec5ab1740629a45f93a8edcf3cc784
Stablest <marlonlocci@hotmail.com>
2023-06-02 23:57:11 -0300

    Added sm and xl screens, added darkMode : class

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,20 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     colors: {
-      "dark-blue": "hsl()209,23%,22%",
+      "dark-blue": "hsl(209,23%,22%)",
       "dark-mode-background": "hsl(207, 26%, 17%)",
       "light-mode-text": "hsl(200, 15%, 8%)",
       "light-mode-input": "hsl(0, 0%, 52%)",
       "light-mode-background": "hsl(0, 0%, 98%)",
       white: "hsl(0, 0%, 100%)",
       black: "#000000",
+    },
+    screens: {
+      sm: "640px",
+      xl: "1280px",
     },
     extend: {},
   },
