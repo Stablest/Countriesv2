6281c8b52f49fc6da541d8efbe284354fdb97741
Stablest <marlonlocci@hotmail.com>
2023-06-03 00:09:43 -0300

    Added body to template. Added dark mode CSS

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,5 +1,8 @@
 import { ReactNode } from "react"
 import { Header } from "./Header"
+import { Nunito_Sans } from 'next/font/google'
+
+const font = Nunito_Sans({ subsets: ['latin'] })
 
 type TemplateProps = {
     children: ReactNode,
@@ -7,12 +10,12 @@ type TemplateProps = {
 
 const Template = ({ children }: TemplateProps) => {
     return (
-        <>
+        <body className={`${font.className} min-h-screen dark:text-white text-black dark:bg-dark-mode-background bg-light-mode-background`}>
             <Header></Header>
-            <div className="bg-light-mode-background overflow-scroll">
+            <div className="dark:bg-dark-mode-background bg-light-mode-background">
                 {children}
             </div>
-        </>
+        </body>
     )
 }
 
