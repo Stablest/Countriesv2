f7bf9622dacd6b2bef9464ab28f48bd05f0248ab
Stablest <marlonlocci@hotmail.com>
2023-06-03 00:03:46 -0300

    Added desktop CSS and dark mode CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,20 +57,24 @@ export default function Home() {
   }, [])
 
   return (
-    <main className='px-4'>
-      <div className='flex items-center px-8 gap-x-4 bg-white drop-shadow-sm h-12 rounded-lg my-8'>
-        <img src="./magnifier.svg" alt="magnifier" className='w-5 h-5' />
-        <input type="text" placeholder='Search for a country...' name="Country" id="" className='bg-white placeholder-light-mode-input' value={inputText} onChange={(e) => handleOnChangeInput(e.target.value)} />
+    <main className='px-4 sm:px-10 xl:px-20 overflow-auto'>
+      <div className='xl:flex xl:justify-between xl:items-center'>
+        <div className='flex items-center px-8 gap-x-4 dark:bg-dark-blue bg-white drop-shadow-md h-12 max-w-md rounded-lg my-8 xl:my-16'>
+          <img src="./magnifier.svg" alt="magnifier" className='w-5 h-5 brightness-[0] saturate-[0%] invert-[100%] hue-rotate-[0deg] contrast-[180%]' />
+          <input type="text" placeholder='Search for a country...' name="Country" id=""
+            className='dark:bg-dark-blue bg-white dark:placeholder:text-white placeholder-light-mode-input'
+            value={inputText} onChange={(e) => handleOnChangeInput(e.target.value)} />
+        </div>
+        <select name="region" id="" className='dark:bg-dark-blue px-4 w-48 h-12 text-center drop-shadow-md rounded-lg' onChange={(e) => handleOnChangeSelect(e.currentTarget.value)}>
+          <option value=''>Filter By Region</option>
+          <option value={REGION.AFRICA} >Africa</option>
+          <option value={REGION.AMERICAS}>America</option>
+          <option value={REGION.ASIA} >Asia</option>
+          <option value={REGION.EUROPE} >Europe</option>
+          <option value={REGION.OCEANIA} >Oceania</option>
+        </select>
       </div>
-      <select name="region" id="" className='px-4 w-48 h-12 text-center drop-shadow-sm rounded-lg' onChange={(e) => handleOnChangeSelect(e.currentTarget.value)}>
-        <option value=''>Filter By Region</option>
-        <option value={REGION.AFRICA} >Africa</option>
-        <option value={REGION.AMERICAS}>America</option>
-        <option value={REGION.ASIA} >Asia</option>
-        <option value={REGION.EUROPE} >Europe</option>
-        <option value={REGION.OCEANIA} >Oceania</option>
-      </select>
-      <div className='flex flex-col items-center'>
+      <div className='flex flex-col items-center  sm:grid sm:grid-cols-[repeat(auto-fill,16rem)] sm:gap-x-4 sm:justify-between'>
         {countries.map((element, index) =>
           <Country key={index}
             flags={element.flags}
