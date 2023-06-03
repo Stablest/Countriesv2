d45b5c1ecaed773921897a2c1fe18da486ac2252
Stablest <marlonlocci@hotmail.com>
2023-06-03 00:08:25 -0300

    Added dark mode CSS. Population is now showing correctly

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react"
 import { Pair } from "./Pair"
+import Link from "next/link"
 
 type CountryProps = {
     children?: ReactNode
@@ -13,19 +14,19 @@ type CountryProps = {
 const Country = ({ flags, name, population, region, capital }: CountryProps) => {
     return (
         <>
-            <a href={`/${name}`} className="bg-white w-64 h-80 mx-auto my-10 rounded-lg drop-shadow-sm overflow-hidden">
-                <div className=" h-3/6 flex justify-center items-center">
+            <Link href={`/${name}`} className="dark:bg-dark-blue bg-white w-64 h-80 my-10 rounded-lg drop-shadow-md overflow-hidden">
+                <div className="h-3/6 flex justify-center items-center">
                     <img src={flags[1]} alt={`Flag of ${name}`} className="h-full w-full" />
                 </div>
                 <div className="px-8 py-4 h-3/6 flex flex-col items-start">
                     <span className="font-bold text-lg">{name}</span>
                     <div className="flex flex-col justify-center mt-3 gap-y-1">
-                        <Pair title="Population:">{population.toString()}</Pair>
+                        <Pair title="Population:">{population.toLocaleString('pt-BR')}</Pair>
                         <Pair title="Region:">{region}</Pair>
                         <Pair title="Capital:">{capital[0]}</Pair>
                     </div>
                 </div>
-            </a>
+            </Link>
         </>
     )
 }
