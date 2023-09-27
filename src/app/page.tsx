bfc3f3c6922645283034ebee6075f8160ecefc7a
Stablest <marlonlocci@hotmail.com>
2023-09-27 13:36:47 -0300

    bug-fix: Removed unused id from input tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
       <div className='xl:flex xl:justify-between xl:items-center'>
         <div className='flex items-center px-8 gap-x-4 dark:bg-dark-blue bg-white drop-shadow-md h-12 max-w-md rounded-lg my-8 xl:my-16'>
           <img src="./magnifier.svg" alt="magnifier" className='w-5 h-5 dark:brightness-[0] dark:saturate-[0%] dark:invert-[100%] dark:hue-rotate-[0deg] dark:contrast-[180%]' />
-          <input type="text" placeholder='Search for a country...' name="Country" id=""
+          <input type="text" placeholder='Search for a country...' name="Country"
             className='dark:bg-dark-blue bg-white dark:placeholder:text-white placeholder-light-mode-input w-full'
             value={inputText} onChange={(e) => handleOnChangeInput(e.target.value)} />
         </div>
