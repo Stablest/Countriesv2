54f5adb1e9a2312fe780defa9dea06a58ad27548
Stablest <marlonlocci@hotmail.com>
2023-09-27 13:39:00 -0300

    misc: Removed unused imports

--- src/utils/hooks/useLocalStorage.ts
+++ src/utils/hooks/useLocalStorage.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const useLocalStorage = <T>(item: string) => {
   const [value, setValue] = useState<T | null>(() => {
